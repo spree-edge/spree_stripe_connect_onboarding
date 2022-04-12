46f853a2f8d15d104f90e68e035e597be12859c3
Vishal <vishal@bluebash.co>
2022-04-12 16:15:09 +0530

    Improve vendor payouts UI

--- app/overrides/add_stripe_view_in_vendor_edit_form.rb
+++ app/overrides/add_stripe_view_in_vendor_edit_form.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 Deface::Override.new(
-  virtual_path:  "spree/admin/vendors/_form",
+  virtual_path:  "spree/admin/vendors/edit",
   name:          "add_stripe_table_in_form",
-  insert_after: "[class='form-group']",
+  insert_after: "fieldset",
   partial: "spree/admin/shared/stripe_details"
 )

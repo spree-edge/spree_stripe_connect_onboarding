304a5562047dda57a5d39a84249d2cf412f2b54b
Vishal <vishal@bluebash.co>
2022-05-06 13:33:37 +0530

    Added stripe onboarding option for vendor

--- app/controllers/spree/admin/vendor_settings_controller_decorator.rb
+++ app/controllers/spree/admin/vendor_settings_controller_decorator.rb
@@ -2,7 +2,19 @@ module Spree
   module Admin
     module VendorSettingsControllerDecorator
       def self.prepended(base)
-        base.before_action :retrieve_stripe_account_info, only: [:edit]
+        base.before_action :retrieve_stripe_account_info, only: [:edit, :account_information]
+      end
+
+      def onboarding
+      end
+
+      def account_information
+      end
+
+      def generate_stripe_account
+        if @vendor.update(vendor_params.except(:image))
+          redirect_to  stripe_connect_url_admin_vendor_path(@vendor)
+        end
       end
 
       private
